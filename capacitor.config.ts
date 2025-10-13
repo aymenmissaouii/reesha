e849f8249d34d439c9b0f979a0d122554342e28c
aymen missaoui <aymenmissaoui56@gmail.com>
2025-10-13 14:16:44 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'dhyati',
+  webDir: 'www'
+};
+
+export default config;
